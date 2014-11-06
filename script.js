3238f7251b75010060e707816e5cb2c8fb13b0fa
Kiryl Dziamura <Kiryl_Dziamura@epam.com>
2014-11-06 17:59:06 +0300

    add player module

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-var rAF = window.requestAnimationFrame;
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 navigator.getUserMedia = (navigator.getUserMedia ||
 						navigator.webkitGetUserMedia ||
@@ -129,6 +128,7 @@ UnknownPleasures.prototype.preRender = function () {
 };
 
 UnknownPleasures.prototype.render = function () {
+	var rAF = window.requestAnimationFrame;
 	var points = this.options.points;
 	var vStep = Math.ceil((this.options.height - 2 * this.options.padding[0]) / this.options.lines);
 	var lines = this.lines;
@@ -250,4 +250,209 @@ UnknownPleasures.prototype.setAnalyser = function (src) {
 	analyser.smoothingTimeConstant = 1;
 	analyser.fftSize = 64;
 	this.analyser = analyser;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function Player (w, h) {
+	var canvas = document.createElement('canvas');
+	canvas.width = w;
+	canvas.height = h;
+	this.canvasCtx = canvas.getContext('2d');
+	this.audioCtx = new AudioContext();
+	this.isPlaying = false;
+	this._pausePosition = 0;
+	this.source = null;
+
+	this.ui = new Player.UI(this);
+}
+
+/**
+ * Set source of player
+ * @param {ArrayBuffer} src source
+ */
+Player.prototype.setSource = function (src) {
+	this.reset();
+	this.source = null;
+	this.audioCtx.decodeAudioData(src, this.connect.bind(this));
+};
+
+Player.prototype.reset = function () {
+	if (this.isPlaying) {
+		this.isPlaying = false;
+		this.source.stop();
+	}
+};
+
+Player.prototype.connect = function (buffer) {
+	var source;
+
+	if (this.source) {
+		this.stop();
+		this.source.disconnect();
+	}
+
+	source = this.audioCtx.createBufferSource();
+
+	source.onended = this.reset.bind(this);
+	source.buffer = buffer || this.source.buffer;
+	this.source = source;
+	this.source.connect(this.audioCtx.destination);
+};
+
+Player.prototype._getTimeByPosition = function (position) {
+	var duration = this.source.buffer.duration;
+	position = position || 0;
+	return duration * position;
+};
+
+Player.prototype._getPosition = function () {
+	var duration = this.source.buffer.duration;
+	var currentTime = this.audioCtx.currentTime - this._startTime;
+
+	return this.isPlaying ? currentTime / duration : this._pausePosition;
+};
+
+Player.prototype.setPosition = function (position) {
+	if (this.isPlaying) {
+		this.play(position);
+	} else {
+		this._pausePosition = position;
+	}
+};
+
+Player.prototype.play = function (position) {
+	position = position !== undefined ? position : this._pausePosition;
+
+	var fromTime = this._getTimeByPosition(position);
+
+	this.connect();
+
+	this.isPlaying = true;
+
+	this._startTime = this.audioCtx.currentTime - fromTime;
+	this.source.start(0, fromTime);
+};
+
+Player.prototype.pause = function () {
+	this._pausePosition = this._getPosition();
+
+	this.reset();
+};
+
+Player.prototype.stop = function () {
+	this._pausePosition = 0;
+
+	this.reset();
+};
+
+
+
+
+
+
+
+
+Player.UI = function (player) {
+	var wrapper = document.createElement('div');
+	var playBtn = document.createElement('div');
+	var progressBarWrapper = document.createElement('div');
+	var progress = document.createElement('div');
+
+	wrapper.classList.add('player');
+	playBtn.classList.add('button');
+	progressBarWrapper.classList.add('progress-bar');
+
+	progressBarWrapper.appendChild(progress);
+
+	wrapper.appendChild(playBtn);
+	wrapper.appendChild(progressBarWrapper);
+
+	this.el = wrapper;
+	this.playBtn = playBtn;
+	this.progressBarWrapper = progressBarWrapper;
+	this.progress = progress;
+
+	this.player = player;
+	this._bindEvents();
+};
+
+Player.UI.prototype._setProgress = function (progress) {
+	this.progress.style.transform = 'translateX(' + (progress*100) + '%)';
+};
+
+Player.UI.prototype._bindEvents = function () {
+	this.playBtn.addEventListener('click', this._playBtnClick.bind(this));
+	this.progressBarWrapper.addEventListener('click', this._progressBarClick.bind(this));
+};
+
+Player.UI.prototype.helpers = {};
+Player.UI.prototype.helpers._getMouseClick = function (e, element) {
+	var totalOffsetX = 0;
+	var totalOffsetY = 0;
+	var canvasX = 0;
+	var canvasY = 0;
+	var currentElement = element;
+
+	do{
+		totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
+		totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
+	}
+	while(currentElement = currentElement.offsetParent)
+
+	canvasX = e.pageX - totalOffsetX;
+	canvasY = e.pageY - totalOffsetY;
+
+	return {x:canvasX, y:canvasY};
+};
+
+// Handlers
+
+Player.UI.prototype._progressBarClick = function (e) {
+	var clickPos = this.helpers._getMouseClick(e, this.progressBarWrapper);
+	var position = clickPos.x / this.progressBarWrapper.offsetWidth;
+	this.player.setPosition(position);
+};
+
+Player.UI.prototype._playBtnClick = function (e) {
+	var player = this.player;
+
+	if (player.isPlaying) {
+		this.player.pause();
+		this.playBtn.classList.remove('pause');
+	} else {
+		this.player.play();
+		this.playBtn.classList.add('pause');
+	}
+};
+
+Player.UI.prototype.render = function () {
+	var rAF = window.requestAnimationFrame;
+	var self = this;
+
+	function draw () {
+		var position = self.player._getPosition();
+
+		self._setProgress(position);
+		rAF(draw);
+	}
+
+	rAF(draw);
 };
